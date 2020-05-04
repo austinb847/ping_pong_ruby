89d380e15511d35b2acd1f3a9b4d19ae7647adbc
austinb847 <austinb847@icloud.com>
2020-05-04 10:23:14 -0700

    meaningful fail test for ping_pong method

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -6,4 +6,8 @@ describe('Integer#ping_pong') do
   it("counts from 1 to a given number") do
     expect(2.ping_pong).to eq([1,2])
   end
+
+  it("adds ping to the array if the number is a multiple of 3") do
+    expect(5.ping_pong).to eq([1,2,"ping",4,5])
+  end
 end
