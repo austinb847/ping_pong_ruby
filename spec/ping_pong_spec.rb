3ee313a9f07c3ecd1c66c2e87ed5deb4c80c3de1
austinb847 <austinb847@icloud.com>
2020-05-04 10:40:45 -0700

    meaningful fail test for ping_pong method

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -7,7 +7,11 @@ describe('Integer#ping_pong') do
     expect(2.ping_pong).to eq([1,2])
   end
 
-  it("adds ping to the array if the number is a multiple of 3") do
+  it("adds ping to the array if the number is divisible 3") do
     expect(5.ping_pong).to eq([1,2,"ping",4,5])
+  end
+
+  it("adds pong to the array if the number is divisible by 5") do
+    expect(5.ping_pong).to eq([1,2,"ping",4,"pong"])
   end
 end
